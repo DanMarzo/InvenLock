be9e5d6aa9e38e513c747342eff65da7025aaace
DanMarzo <marzogildan@gmail.com>
2023-01-30 23:44:13 -0300

    Tabelas Sql Uso próprio

--- Querrys/Geral.sql
+++ Querrys/Geral.sql
@@ -2,4 +2,12 @@ SELECT * FROM Equipamentos
 
 SELECT * FROM Funcionarios
 
-spHelp Funcionarios
+SELECT * FROM Ocorrencias
+
+SELECT * FROM ConsertoEquips
+
+sp_help Funcionarios
+sp_help Equipamentos
+sp_help Ocorrencias
+sp_help ConsertoEquips
+                       
