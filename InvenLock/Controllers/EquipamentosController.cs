e7bb272be5d21c35fd18607171d0e6a8bb5a48c1
DanMarzo <marzogildan@gmail.com>
2023-02-04 19:48:16 -0300

    Melhora na busca, e otimizando o codigo

--- InvenLock/Controllers/EquipamentosController.cs
+++ InvenLock/Controllers/EquipamentosController.cs
@@ -1,6 +1,8 @@
 using InvenLock.Data;
 using InvenLock.Models;
+using InvenLock.Utils;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InvenLock.Controllers;
 
@@ -16,6 +18,19 @@ public class EquipamentosController : ControllerBase
     {
         try
         {
+            VerificaDados verifica = equipamento.MarcaEquipamento is null || equipamento.FuncionarioRecebedor is null ?
+                throw new Exception("Verifique os campos obrigatorios")
+                : new();
+
+            equipamento.FuncionarioRecebedor = verifica.ConsertaCpf(equipamento.FuncionarioRecebedor);
+
+            Funcionario existeFunc = !verifica.RecebeCpf(equipamento.FuncionarioRecebedor) ?
+                throw new Exception("Verifique o CPF")
+                : await _context.Funcionarios.FirstOrDefaultAsync(x => x.FuncionarioCPF == equipamento.FuncionarioRecebedor);
+
+            if (existeFunc is null) return NotFound("Funcionario não cadastrado na empresa");
+            if (existeFunc.Ativo is false) throw new Exception("Funcionario está inativo");
+
             bool aceito = false;
             string pk = "";
             while(aceito != true)
@@ -30,12 +45,12 @@ public class EquipamentosController : ControllerBase
             await _context.Equipamentos.AddAsync(equipamento);
             await _context.SaveChangesAsync(); 
 
-            return Ok(equipamento);
-            //return Ok();
+            return Ok($"Codigo Interno: {equipamento.CodigoInterno}");
         }
         catch (Exception ex)
         {
             return BadRequest(ex.Message);
         }
     }
+
 }
