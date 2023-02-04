46cd5b2e265aeddc8845535ef33284f582fe6380
DanMarzo <marzogildan@gmail.com>
2023-02-04 19:50:00 -0300

    Incluindo novas EndPoints

--- InvenLock/Controllers/FuncionariosController.cs
+++ InvenLock/Controllers/FuncionariosController.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using Azure.Core;
 using InvenLock.Data;
 using InvenLock.Models;
 using InvenLock.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace InvenLock.Controllers;
 
@@ -22,11 +25,17 @@ public class FuncionariosController : ControllerBase
     {
         try
         {
-            VerificaDados verifica = new();
-            if(!verifica.RecebeCpf(funcionario.FuncionarioCPF))
-                throw new Exception("Verifique o CPF");
-            Funcionario confirmaCpf = await _context.Funcionarios
+            VerificaDados verifica = funcionario is null || funcionario.FuncionarioCPF is null ?
+                throw new Exception("Verifique os campos obrigatorios")
+                : new();
+
+            funcionario.FuncionarioCPF = verifica.ConsertaCpf(funcionario.FuncionarioCPF);
+
+            Funcionario confirmaCpf = !verifica.RecebeCpf(funcionario.FuncionarioCPF) ?
+                throw new Exception("Verifique o CPF")
+                : await _context.Funcionarios
                 .FirstOrDefaultAsync(x => x.FuncionarioCPF == funcionario.FuncionarioCPF);
+
             if(confirmaCpf != null)
                 throw new Exception("CPF já esta sendo utilizado");
 
@@ -57,23 +66,18 @@ public class FuncionariosController : ControllerBase
     {
         try
         {
-            if(funcionario is null || funcionario.FuncionarioCPF is null)
-                throw new Exception("O mesmo não pode ser nulo");
-            
-            VerificaDados verifica = new();
+            VerificaDados verifica = funcionario.Ativo is null || funcionario.FuncionarioCPF is null ?
+                throw new Exception("Campos Obrigatorios")
+                : new();
 
-            if(!verifica.RecebeCpf(funcionario.FuncionarioCPF))
-                throw new Exception("Verifique o CPF informado");
-            
-            Funcionario consulta = await _context.Funcionarios
+            funcionario.FuncionarioCPF = verifica.ConsertaCpf(funcionario.FuncionarioCPF);
+
+            Funcionario consulta = !verifica.RecebeCpf(funcionario.FuncionarioCPF) ? 
+                throw new Exception("Verifique o CPF informado")
+                : await _context.Funcionarios
                 .FirstOrDefaultAsync(f => f.FuncionarioCPF == funcionario.FuncionarioCPF);
-            
-            if(consulta is null)
-                return NotFound("Funcionario não encontrado");
 
-            //Como provavelmente no app que vai consumir vai usar um dropdown entao isso talvez nem funcione kkk
-            if(funcionario.Ativo == consulta.Ativo)
-                throw new Exception("Você esta tentando mudar a situação para a mesma atual");
+            if (consulta is null) return NotFound("Funcionario não encontrado");
 
             consulta.Ativo = funcionario.Ativo;
 
@@ -85,7 +89,8 @@ public class FuncionariosController : ControllerBase
                 consulta.DataDemissao = null; //Olha eu sei que isso ta estranho kkkk, mas, logo a frente eu penso em criar uma tabela para esse tipo de registro
             }
             string fraseSaida = consulta.Ativo == false ?
-                $"{consulta.NomeFuncionario} desligado com sucesso! Na data {consulta.DataDemissao}" : $"{consulta.NomeFuncionario} ativo com sucesso! Na data {consulta.DataAdmissao}";
+                $"{consulta.NomeFuncionario} desligado com sucesso! Na data {consulta.DataDemissao}" 
+                : $"{consulta.NomeFuncionario} ativo com sucesso! Na data {consulta.DataAdmissao}";
             
             _context.Funcionarios.Update(consulta);
             await _context.SaveChangesAsync();
@@ -105,5 +110,67 @@ public class FuncionariosController : ControllerBase
         if(todos is null)
             return NotFound("Nenhum funcionario cadastrado");
         return Ok(todos);
+    }
+
+    [HttpPost("BuscaCPF")]
+    public async Task<IActionResult> BuscaPorCFF(string cpf)
+    {
+        try
+        {
+            VerificaDados verificaDados =
+                cpf is null ?
+                throw new Exception("CPF Não pode ser nulo")
+                : new();
+            cpf = verificaDados.ConsertaCpf(cpf);
+
+            Funcionario existeFun =
+                !verificaDados.RecebeCpf(cpf) ?
+                throw new Exception("Verifique o CPF inserido")
+                : await _context.Funcionarios
+                .FirstOrDefaultAsync(x => x.FuncionarioCPF == cpf);
+
+            if (existeFun != null) return Ok(existeFun);
+            return NotFound(($"CPF: {cpf} não encontrado"));
+            
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+
+    [HttpPost("Contato")]
+    public async Task<IActionResult> ContatosAsync(ContatoFuncionario contatoFuncionario)
+    {
+        try
+        {
+            VerificaDados verificaDados = 
+                contatoFuncionario.CPF is null ? 
+                throw new Exception("CPF Não pode ser nulo") 
+                : new();
+
+            contatoFuncionario.CPF = verificaDados.ConsertaCpf(contatoFuncionario.CPF);
+
+            Funcionario existeFun =
+                !verificaDados.RecebeCpf(contatoFuncionario.CPF) ?
+                throw new Exception("Verifique o CPF inserido")
+                : await _context.Funcionarios
+                .FirstOrDefaultAsync(x => x.FuncionarioCPF == contatoFuncionario.CPF);
+
+            if (existeFun is null) return NotFound("CPF não cadastrado");
+            if (existeFun.Ativo == false) throw new Exception("Funcionario inativo");
+
+            contatoFuncionario.FuncionarioId = existeFun.FuncionarioId;
+
+            await _context.ContatoFuncionarios.AddAsync(contatoFuncionario);
+            int linhasAfetadas =  await _context.SaveChangesAsync();
+
+            return Ok(linhasAfetadas);
+
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 }
